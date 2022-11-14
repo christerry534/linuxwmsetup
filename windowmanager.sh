8d5d420eb8077ba66915644b09e686b9d98d6ac8
christerry534 <87517708+christerry534@users.noreply.github.com>
2022-11-14 07:33:53 +0000

    Update windowmanager.sh

--- windowmanager.sh
+++ windowmanager.sh
@@ -13,5 +13,7 @@ apt update && apt install brave-browser -y
 apt install xterm -y
 apt install lxterminal -y
 apt install xinit -y
-apt install openvpn3 -y
-echo "Loging Using startx"
+#apt install openvpn3 -y
+echo --------------------
+echo "Login Using startx"
+echo --------------------
